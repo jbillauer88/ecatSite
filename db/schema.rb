cec46da545473f94698553f84d24afa9fe528b93
Jeremy <jeremy.billauer@gmail.com>
2018-08-16 10:35:43 -0700

    Added users and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_055005) do
+ActiveRecord::Schema.define(version: 2018_08_15_062328) do
 
   create_table "samples", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_08_15_055005) do
     t.text "addNotes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_samples_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
