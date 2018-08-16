fa37250f7b92e40834096a44289d100ead4a76e4
Jeremy <jeremy.billauer@gmail.com>
2018-08-16 12:42:38 -0700

    Adding additional static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "about" , to:"pages#about"
+  get "contact" , to:"pages#contact"
+  get "faq" , to:"pages#faq"
+
   devise_for :users
   resources :samples
   root to: "samples#index"
